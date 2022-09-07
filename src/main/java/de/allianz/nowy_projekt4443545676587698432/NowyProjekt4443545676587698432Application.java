5a9f5c471e15e2d4016523a15ce415beb2d9d3ee
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-09-07 12:44:13 +0200

    Create gradle project

---
+++ src/main/java/de/allianz/nowy_projekt4443545676587698432/NowyProjekt4443545676587698432Application.java
@@ -0,0 +1,13 @@
+package de.allianz.nowy_projekt4443545676587698432;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowyProjekt4443545676587698432Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowyProjekt4443545676587698432Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/nowy_projekt4443545676587698432/NowyProjekt4443545676587698432ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.nowy_projekt4443545676587698432;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowyProjekt4443545676587698432ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
